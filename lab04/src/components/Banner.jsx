ea857787c2255e405247a9482c18f617d92f6a02
Ernest Przybyl <ernestprzybyl@gmail.com>
2022-03-21 17:00:17 +0100

    repair banner

--- lab04/src/components/Banner.jsx
+++ lab04/src/components/Banner.jsx
@@ -6,17 +6,16 @@ import "../assets/images/drone_banner2.jpg";
 export default function Banner() {
   return (
     <div className="banner row">
-      <div className="banner__mask row col-12">
-        <div className="col-0 col-md-1 d-none d-md-block"></div>
-        <div className="col-12 col-md-8 d-flex flex-column justify-content-center">
-          <h2 className="display-4 fw-bold text-white">Podróżowanie</h2>
-          <hr className="hr" />
+      <div className="banner__mask">
+        <div className="h-100 d-flex justify-content-center align-items-center flex-column">
+          <h2 className="display-4 fw-bold text-white pt-5">Drony</h2>
+          <hr className="col-8 bg-white" />
           <h4 className="text-white my4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
             consequuntu r.
           </h4>
           <button type="button" className="btn btn-outline-light">
-            Dowiedz się więcej<i className="fas fa-book ml-2"></i>
+            Dowiedz się więcej<i className="fas fa-book ms-2"></i>
           </button>
         </div>
       </div>
