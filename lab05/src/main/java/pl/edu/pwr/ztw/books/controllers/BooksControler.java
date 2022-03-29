51a4db90688dbc3684c34d96cb6a1c5999997d18
Ernest Przybyl <ernestprzybyl@gmail.com>
2022-03-29 16:35:52 +0200

    implement full books CRUD

--- lab05/src/main/java/pl/edu/pwr/ztw/books/controllers/BooksControler.java
+++ lab05/src/main/java/pl/edu/pwr/ztw/books/controllers/BooksControler.java
@@ -3,11 +3,9 @@ package pl.edu.pwr.ztw.books.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import pl.edu.pwr.ztw.books.models.Book;
 import pl.edu.pwr.ztw.books.services.IBooksService;
 
 @RestController
@@ -24,6 +22,32 @@ public class BooksControler {
     @RequestMapping(value = "/books/{id}", method = RequestMethod.GET)
     public ResponseEntity<Object> getBook(@PathVariable("id") int id){
         return new ResponseEntity<>(booksService.getBook(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/books/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteBook(@PathVariable int id){
+        if(booksService.deleteBook(id))
+        {
+            return new ResponseEntity(HttpStatus.OK);
+        };
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping(value = "/books", method = RequestMethod.PATCH)
+    public ResponseEntity updateBook(@RequestBody Book book){
+        if(booksService.updateBook(book))
+        {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        };
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping(value = "/books", method = RequestMethod.POST)
+    public ResponseEntity addBook(@RequestBody Book book){
+        if(booksService.addBook(book)){
+            return new ResponseEntity(HttpStatus.CREATED);
+        };
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
 }
--- lab05/src/main/java/pl/edu/pwr/ztw/books/services/IBooksService.java
+++ lab05/src/main/java/pl/edu/pwr/ztw/books/services/IBooksService.java
@@ -7,4 +7,7 @@ import java.util.Collection;
 public interface IBooksService {
     public abstract Collection<Book> getBooks();
     public abstract Book getBook(int id);
+    public abstract boolean deleteBook(int id);
+    public abstract boolean addBook(Book book);
+    public abstract boolean updateBook(Book book);
 }
