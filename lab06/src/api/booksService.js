11bc73b16bd967fd44327e49574b085b81ad742b
Ernest Przybyl <ernestprzybyl@gmail.com>
2022-04-05 12:28:16 +0200

    add books service

---
+++ lab06/src/api/booksService.js
@@ -0,0 +1,19 @@
+async function getAllBooks() {
+  //TODO
+}
+
+async function getBookById(id) {
+  //TODO
+}
+
+async function deleteBookById(id) {
+  //TODO
+}
+
+async function addBook(book) {
+  //TODO
+}
+
+async function updateBook(book) {
+  //TODO
+}
