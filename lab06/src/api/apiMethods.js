86b3aa1dffe6479979516b02e98484d411fd7d30
Kamil-Lontkowski <kamillont14@gmail.com>
2022-04-05 12:58:42 +0200

    Api booksService

---
+++ lab06/src/api/apiMethods.js
@@ -0,0 +1,41 @@
+export function urlSearchParams(url = "", params = {}) {
+  const result = new URL(url);
+  result.search = new URLSearchParams(params).toString();
+
+  return result.toString();
+}
+
+export async function postData(url = "", data = {}, params = {}) {
+  url = urlSearchParams(url, params);
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+export async function patchData(url = "", data = {}, params = {}) {
+  url = urlSearchParams(url, params);
+
+  const response = await fetch(url, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+export async function deleteData(url = "", params = {}) {
+  url = urlSearchParams(url, params);
+
+  const response = await fetch(url, {
+    method: "DELETE",
+  });
+  return response.json();
+}
--- lab06/src/api/booksService.js
+++ lab06/src/api/booksService.js
@@ -1,19 +1,34 @@
-async function getAllBooks() {
-  //TODO
-}
+import { postData, patchData, urlSearchParams, deleteData } from "./apiMethods";
+export class booksService {
 
-async function getBookById(id) {
-  //TODO
-}
+  constructor(baseURL = "http://localhost:8080") {
+    if (baseURL.endsWith("/")) {
+      this.baseURL = baseURL + "books";
+    } else {
+      this.baseURL = baseURL + "/books";
+    }
+  }
 
-async function deleteBookById(id) {
-  //TODO
-}
+  async function getAllBooks() {
+    const response = await fetch(this.baseURL);
+    return response.json();
+  }
+  
+  async function getBookById(id) {
+    const response = await fetch(this.baseURL + "/" + id.toString())
+    return response.json();
+  }
+  
+  async function deleteBookById(id) {
+    return deleteData(this.baseURL + "/" + id.toString())
+  }
+  
+  async function addBook(book) {
+    return postData(this.baseURL, book);
+  }
+  
+  async function updateBook(book) {
+    return patchData(this.baseURL, book)
+  }
 
-async function addBook(book) {
-  //TODO
-}
-
-async function updateBook(book) {
-  //TODO
 }
