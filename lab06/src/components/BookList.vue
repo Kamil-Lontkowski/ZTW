99774950f7d65b1123c5be445d12103f484ef944
Ernest Przybyl <ernestprzybyl@gmail.com>
2022-04-05 13:03:07 +0200

    books save

---
+++ lab06/src/components/BookList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    yee
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">Tytol</th>
+          <th scope="col">Autor</th>
+          <th scope="col">Strony</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: "books-list",
+  props: {
+    booksList: Array,
+  },
+};
+</script>
--- lab06/src/views/MainBookPage.vue
+++ lab06/src/views/MainBookPage.vue
@@ -1,3 +1,39 @@
+<script>
+import BookList from "../components/BookList.vue";
+const books = [
+  {
+    id: 1,
+    title: "Potop",
+    author: {
+      id: 1,
+      firstName: "Henryk",
+      lastName: "Sienkiewicz",
+    },
+    pages: 936,
+  },
+  {
+    id: 2,
+    title: "Wesele",
+    author: {
+      id: 2,
+      firstName: "Stanisław",
+      lastName: "Reymont",
+    },
+    pages: 150,
+  },
+  {
+    id: 3,
+    title: "Dziady",
+    author: {
+      id: 3,
+      firstName: "Adam",
+      lastName: "Mickiewicz",
+    },
+    pages: 292,
+  },
+];
+</script>
 <template>
-  Main book page
+  Main book page 2
+  <BookList :books="books" />
 </template>
