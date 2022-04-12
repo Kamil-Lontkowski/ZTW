448932cf62c99ccd8bbf5bd559d653c1fe69c3ed
Ernest Przybyl <ernestprzybyl@gmail.com>
2022-04-12 16:17:09 +0200

    add prettier

--- lab07/src/resolvers.js
+++ lab07/src/resolvers.js
@@ -7,26 +7,26 @@ export const resolvers = {
     user: async (parent, args) => {
       const id = parseInt(args.id);
       const users = await getUsers();
-      const found_user = users.find((user) => user.id === id);
+      const found_user = users.find(user => user.id === id);
       return found_user;
     },
     todo: async (parent, args) => {
       const id = parseInt(args.id);
       const todos = await getTodos();
-      return todos.find((todo) => todo.id === id);
+      return todos.find(todo => todo.id === id);
     },
   },
   User: {
-    todos: async (parent) => {
+    todos: async parent => {
       const todos = await getTodos();
-      const user_todos = todos.filter((todo) => todo.userId === parent.id);
+      const user_todos = todos.filter(todo => todo.userId === parent.id);
       return user_todos;
     },
   },
   ToDoItem: {
-    user: async (parent) => {
+    user: async parent => {
       const users = await getUsers();
-      const user = users.find((user) => user.id === parent.userId);
+      const user = users.find(user => user.id === parent.userId);
       return user;
     },
   },
