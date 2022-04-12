2f49369e64feab7074056ca1694927a84dcd1233
Ernest Przybyl <ernestprzybyl@gmail.com>
2022-04-12 15:51:48 +0200

    add eslint

--- lab07/src/resolvers.js
+++ lab07/src/resolvers.js
@@ -4,13 +4,13 @@ export const resolvers = {
   Query: {
     users: () => getUsers(),
     todos: () => getTodos(),
-    user: async (parent, args, context, info) => {
+    user: async (parent, args) => {
       const id = parseInt(args.id);
       const users = await getUsers();
       const found_user = users.find((user) => user.id === id);
       return found_user;
     },
-    todo: async (parent, args, context, info) => {
+    todo: async (parent, args) => {
       const id = parseInt(args.id);
       const todos = await getTodos();
       return todos.find((todo) => todo.id === id);
