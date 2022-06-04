2e5607c7a9d30db0d872676c64aa32e21d2aeecf
Kamil-Lontkowski <kamillont14@gmail.com>
2022-06-04 14:17:30 +0200

    Added solution for lab12, signing with SSO

--- lab05/src/test/java/pl/edu/pwr/ztw/books/BooksApplicationTests.java
+++ lab05/src/test/java/pl/edu/pwr/ztw/books/BooksApplicationTests.java
@@ -1,13 +1,13 @@
-package pl.edu.pwr.ztw.books;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BooksApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package pl.edu.pwr.ztw.books;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BooksApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
